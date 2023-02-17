ad792786a893391253a9627363eb4845876bb9d6
sebounti <sebastienlenne@gmail.com>
2023-02-17 03:07:21 -0800

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* main-Entry point
-* Return: Always 0 success
+* main - Entry point
+* Return: Always 0 (success)
 */
 int main(void)
 {
 printf("witch proprer grammar, but the outcome is a piece of art,\n");
-return (O);
+return (0);
 }
