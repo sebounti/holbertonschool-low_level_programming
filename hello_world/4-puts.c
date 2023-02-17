be21cb9ab6fd6b48a21b7e7413e577e8c870eea4
sebounti <sebastienlenne@gmail.com>
2023-02-17 00:34:12 -0800

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
-
+  
 /*Program that displays a message 
- returns to the line with zero */
+* returns to the line with zero
+*/
 
 int maim(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
-
