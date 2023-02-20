c3d1c42b32b99bfb5f4aed47ea7ef478f3ea030c
sebounti <sebastienlenne@gmail.com>
2023-02-20 15:14:59 -0800

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char cba;
+		for (cba = 'z'; cba >= 'a'; cba--)
+
+		putchar(cba);
+	putchar('\n');
+return (0);
+}
