eca2947200c0b1a8f2046c8c9e47741d29320d33
sebounti <sebastienlenne@gmail.com>
2023-02-20 12:28:43 -0800

    3-print_alphabets.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char abc, ABC;
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+	{	putchar(abc);
+	}
+
+	for (ABC = 'A'; ABC <= 'Z'; ABC++)
+	{	putchar(ABC);
+	}
+
+putchar('\n');
+return (0);
+}
+
