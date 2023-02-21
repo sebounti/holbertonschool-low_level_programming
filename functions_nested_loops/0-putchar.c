fe026f91d97646c21efbc702309b478363a1e596
sebounti <sebastienlenne@gmail.com>
2023-02-21 06:04:13 -0800

    functions_nested_loops/

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('a');
+putchar('r');
+
+putchar ('\n');
+return (0);
+}
