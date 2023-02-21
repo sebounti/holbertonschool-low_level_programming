22e9e42f30c56ba887ea982f5c5f707602bbb4ed
sebounti <sebastienlenne@gmail.com>
2023-02-21 02:21:19 -0800

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num;
+
+for (num = 0 ; num <= 9 ; num++)
+{	if
+	(num == 0);
+else
+{
+	putchar(',');
+	putchar(' ');
+}
+
+putchar(num + '0');
+}
+putchar('\n');
+return (0);
+}
+
