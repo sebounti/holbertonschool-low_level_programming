2e270aa81804fe44bf4b8777dd2d32c002e7022b
sebounti <sebastienlenne@gmail.com>
2023-02-22 09:02:54 -0800

    7-print_last_digit.c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - check the code for Holberton School students.
+ *
+ * @c: is a int variable
+ *
+ * Return: lastdig
+ */
+int print_last_digit(int c)
+{
+	int lastdig = c % 10;
+
+	if (lastdig < 0)
+	lastdig = lastdig * (-1);
+
+	_putchar(lastdig + '0');
+
+return (lastdig);
+}
