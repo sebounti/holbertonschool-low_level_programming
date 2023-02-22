e47ab5cf9ca4468e713b0cf994f97e290b718398
sebounti <sebastienlenne@gmail.com>
2023-02-21 21:48:02 -0800

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet_x10-prints alphabet.
+ *
+ * Return: 0 Success
+ *
+ */
+void print_alphabet_x10(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+
+{
+	char (alpha);
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+
+_putchar(alpha);
+_putchar('\n');
+}
+}
