f0026e712b2bc705750d3108e3b329cb4384da67
sebounti <sebastienlenne@gmail.com>
2023-03-03 12:38:27 -0800

    6 cap string

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+* cap_string - change lowercase letters to uppercase
+* @str: string variable
+* Return: Always 0
+*/
+
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+
+		for (; str[i] != '\0'; i++)
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				if (str[i - 1] == ' ' ||
+				str[i - 1] == ',' ||
+				str[i - 1] == ';' ||
+				str[i - 1] == '.' ||
+				str[i - 1] == '!' ||
+				str[i - 1] == '?' ||
+				str[i - 1] == '"' ||
+				str[i - 1] == '.' ||
+				str[i - 1] == '(' ||
+				str[i - 1] == ' ' ||
+				str[i - 1] == ')' ||
+				str[i - 1] == '{' ||
+				str[i - 1] == '}' ||
+				str[i - 1] == '9')
+				{
+				str[i] = str[i] - 32;
+				}
+			}
+		}
+	}
+	return (str);
+}
