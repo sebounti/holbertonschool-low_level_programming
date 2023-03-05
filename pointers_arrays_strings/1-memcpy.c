e1d2095dca6266a40be5c2795b8c27c8f81dad74
sebounti <sebastienlenne@gmail.com>
2023-03-05 10:55:19 -0800

    exercice 1->5 P3 Pointers

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+* _memcpy - copy memory area
+* @dest: destination
+* @src: original area
+* @n: num of bytes
+* Return: Always 0
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (; i < n; i++)
+		dest[i] = *src;
+
+	return (dest);
+}
