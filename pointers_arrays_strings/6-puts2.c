33afd7f0273388973630b9f1d1e61a4bf5c1f087
sebounti <sebastienlenne@gmail.com>
2023-03-05 14:27:50 -0800

    puts2

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,20 +1,23 @@
 #include "main.h"
-#include "stdio.h"
-
 /**
- * puts2- prints a string in reverse order
- *
+ * puts2 - prints every other character of a string
  * @str: the string to print
- *
  */
 
 void puts2(char *str)
 {
-	while (*str != '\0')
-	{
-	_putchar (*str);
-	str += 2;
-	}
+	int l = 0;
+	int x = 0;
 
-	_putchar ('\n');
+	for (; str[l] != '\0'; l++)
+	{
+		;
+	}
+	l--;
+	while (x <= l)
+	{
+		_putchar(str[x]);
+		x = x + 2;
+	}
+	_putchar('\n');
 }
