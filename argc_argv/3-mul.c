2339c4849f3beabb8ff6b960049b3f6259bd1a09
sebounti <sebastienlenne@gmail.com>
2023-03-08 07:29:34 -0800

    mul

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -7,16 +7,13 @@
  * @argv: contains the name of the command.
  * Return: 0.
  */
-
 int main(int argc, char *argv[])
-{	
-
-    if (argc != 3)
-    {
+{
+	if (argc != 3)
+	{
 	printf("Error\n");
 	return (1);
 	}
-	printf("%d\n",atoi(argv [1]) * atoi (argv [2]));
-	      
-	return(0);
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
 }
