078aca361eb1d96532d65ddb35b27560f3286720
sebounti <sebastienlenne@gmail.com>
2023-03-08 04:39:09 -0800

    args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *  main - program that prints its name, followed by a new line.
+ *  @argc: Variable that contains the number of arguments
+ *  @argv: The variable that contains the number of arguments
+ *
+ *  Return: always 0
+ */
+
+int main(int argc, char __attribute__((__unused__))*argv[])
+{
+		printf("%d\n", argc - 1);
+	return (0);
+}
+
