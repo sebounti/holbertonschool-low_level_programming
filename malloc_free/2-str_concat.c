6c1cc2a4f705df91439136ac2771f7070989119c
sebounti <sebastienlenne@gmail.com>
2023-03-14 12:26:08 +0100

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -14,39 +14,39 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-    int len1 = 0;
-    int len2 = 0;
-    char *result;
-    int i;
-    int j = 0;
-    
-    if (s1 == NULL)
-    s1 = "";
+	int len1 = 0;
+	int len2 = 0;
+	char *result;
+	int i;
+	int j = 0;
 
-    if(s2 == NULL)
-    s2 = "";
+	if (s1 == NULL)
+	s1 = "";
 
-    while (s1[len1] != '\0')
-    {
-        len1 ++;
-    }
-    while (s2 [len2] != '\0')
-    {
-        len2 ++;
-    }
+	if (s2 == NULL)
+	s2 = "";
 
-    result = malloc((len1 + len2) * sizeof(char));
+	while (s1[len1] != '\0')
+	{
+		len1++;
+	}
+	while (s2[len2] != '\0')
+	{
+		len2++;
+	}
 
-    if (result == 0)
-    return (0);
+	result = malloc((len1 + len2) * sizeof(char));
 
-    for (i = 0; i < len1; i++)
-    {
-        result[j++] = s1[i];
-    }
-    for (i = 0; i < len2; i++)
-    {
-        result[j++] = s2 [i];
-    }
-    return (result);
+	if (result == 0)
+	return (0);
+
+	for (i = 0; i < len1; i++)
+	{
+		result[j++] = s1[i];
+	}
+	for (i = 0; i < len2; i++)
+	{
+		result[j++] = s2[i];
+	}
+	return (result);
 }
