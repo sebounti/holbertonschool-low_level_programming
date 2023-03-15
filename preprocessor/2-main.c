cab91cd74d6cfd8b088af76a07834efb7d517634
sebounti <sebastienlenne@gmail.com>
2023-03-15 10:51:41 +0100

    readme

---
+++ preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - 'function to print the name of the program
+ * @void: it takes not arguments
+ *
+ * Return: 0
+**/
+int main(void)
+{
+	printf("%s\x0a", __FILE__);
+
+	return (0);
+}
