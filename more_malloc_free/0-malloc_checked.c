a3ac48c72d04459946e2b17266520962b317c827
sebounti <sebastienlenne@gmail.com>
2023-03-15 13:30:41 +0100

    o-malloc

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+
+#include <stdlib.h>
+#include "main.h"
+/**
+ * malloc_checked - allocates memory with malloc
+ * @b: memory to allocate
+ * Return: pointer to allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *a = malloc(b);
+
+	if (a == NULL)
+		exit(98);
+	return (a);
+}
