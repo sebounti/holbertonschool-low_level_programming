be3d1296f3a195c559697fa0c1ffcc5ca113002c
sebounti <sebastienlenne@gmail.com>
2023-03-23 21:18:03 +0100

    op functions

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -47,9 +47,13 @@ int op_mul(int a, int b)
 
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
-
 /**
  * op_mod - modular operator
  * @a: numerator
@@ -59,6 +63,10 @@ int op_div(int a, int b)
 
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
-
