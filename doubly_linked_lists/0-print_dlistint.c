20640dd32539c7bd7178acc304329fbdd81c39f7
sebounti <sebastienlenne@gmail.com>
2023-04-04 14:55:13 +0200

    creation directory doubly link/fichier readme /task O

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a linked list
+ * @h: pointer to the list_t list to print
+ *
+ * Return: the number of nodes printed
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+
+	size_t s = 0;
+
+	while (h)
+	{
+		f(!h->n)
+		printf("[0] (nil)\n");
+		else
+		printf("[%d]\n", h->n);
+		h = h->next;
+		s++;
+	}
+
+	return (s);
+}
