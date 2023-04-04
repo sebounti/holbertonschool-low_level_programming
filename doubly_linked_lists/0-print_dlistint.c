b70ff12cd69a4e2e70808bbf7ad511c039b5e29d
sebounti <sebastienlenne@gmail.com>
2023-04-04 22:43:31 +0200

    maj fichier 0-print_dlistint.c

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "lists.h"
 
 /**
@@ -11,11 +10,14 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t n_nodes = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		printf("[%d]\n", h->n);
-		n_nodes++;
 		h = h->next;
+		n_nodes++;
+
 	}
 	return (n_nodes);
 }
+
+
