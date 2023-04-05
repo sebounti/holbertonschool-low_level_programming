76ad1e7f1d2af4d5fc53e26150834233cbb668b7
sebounti <sebastienlenne@gmail.com>
2023-04-05 10:58:11 +0200

    test task 1

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -3,7 +3,6 @@
 /**
  * dlistint_len - returns the number of elements in a linked lists
  * @h: linked list of type listint_t to traverse
- *
  * Return: number of nodes
  */
 size_t dlistint_len(const dlistint_t *h)
