1491881384214e56575ce19a7eb7ec7cf2ef1141
sebounti <sebastienlenne@gmail.com>
2023-04-05 11:24:03 +0200

    test task 2

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -8,14 +8,16 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new;
-
-	new = malloc(sizeof(dlistint_t));
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+	
 	if (!new)
-	return (NULL);
+		return (NULL);
 
 	new->n = n;
+	new->prev = NULL;
 	new->next = *head;
+	if (*head != NULL)
+		(*head)->prev = new;
 	*head = new;
 
 	return (new);
