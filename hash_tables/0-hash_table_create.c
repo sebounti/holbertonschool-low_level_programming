3451059a7243cb21c8cf2690d167376397ca3a62
sebounti <sebastienlenne@gmail.com>
2023-04-12 17:53:23 +0200

    0-hash_table_create.c/betty ok

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,10 +1,10 @@
 #include "hash_tables.h"
 /**
- * 
- * 
- * 
- * 
- * 
+ * hash_table_create - creates a hash table.
+ * @size: the size of the array.
+ *
+ * return : null if an error occurs.
+ * or else - a pointer to the new hash table.
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
@@ -17,9 +17,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	ht->size = size;
 	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if(ht->array == NULL)
+	if (ht->array == NULL)
 		return (NULL);
-	for(i = 0 ; i < size; i++)
+	for (i = 0 ; i < size; i++)
 		ht->array[i] = NULL;
 
 	return (ht);
